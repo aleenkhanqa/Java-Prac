2a00fe3fcc35796d62e704154a886354e038e664
Aleen <aleenkhanqa@gmail.com>
2022-04-11 19:22:35 -0400

    this is testing case

---
+++ src/com/bangladesh/dev/qa/RampuraDhaka.java
@@ -0,0 +1,9 @@
+package com.bangladesh.dev.qa;
+
+public class RampuraDhaka {
+    public static void main(String[] args) {
+
+        System.out.println("My permanent resident is BANGLADESH");
+
+    }
+}
