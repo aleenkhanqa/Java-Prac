d664129c9d8917f2112ce3a513555d60f00914d5
Aleen <aleenkhanqa@gmail.com>
2022-04-11 18:54:28 -0400

    april 11th 2022

--- src/com/bangladesh/dev/qa1/TestCaseDemo.java
+++ src/com/bangladesh/dev/qa1/TestCaseDemo.java
@@ -1,6 +1,6 @@
 package com.bangladesh.dev.qa1;
 
-public class BirdDemo {
+public class TestCaseDemo {
 
     public static void main(String[] args) {
         System.out.println("I am trying to solve a little");
